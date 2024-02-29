cec7fa61d4eaa34b6e00b04b9a2d0db8f8e16353
Janis <91672668+jk019@users.noreply.github.com>
2024-02-29 22:05:18 +0100

    delete all

--- Frontend/.netlify/server/entries/pages/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1 data-svelte-h="svelte-yyjjjs">Welcome to SvelteKit</h1> <p data-svelte-h="svelte-1oxfxe9">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation!!</p>`;
-});
-export {
-  Page as default
-};
--- Frontend/.netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.HCHbx8II.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.fi1e1Nz5.js","_app/immutable/chunks/entry.aG30lfGH.js"];
-export const stylesheets = [];
-export const fonts = [];
