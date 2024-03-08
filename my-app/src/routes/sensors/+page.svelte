a89e013616a06eb17eff2d89fe744123e9930735
Janis <91672668+jk019@users.noreply.github.com>
2024-03-08 17:58:38 +0100

    now it works with data in the cards

--- my-app/src/routes/sensors/+page.svelte
+++ my-app/src/routes/sensors/+page.svelte
@@ -13,16 +13,6 @@
 
 <h1>Sensoren</h1>
 
-<h3>Sections</h3>
-{#each data.sections as section}
-	<p>{section.section_name}</p>
-	{#each data.sensors as sensor}
-	{#if sensor.section_id == section._id}
-		<p>{sensor.value}</p>{/if}
-{/each}
-{/each}
-
-
 
 <nav class="nav" style="margin-bottom: 20px;">
 	<a
@@ -51,154 +41,46 @@
 <!-- Conditional Content Rendering -->
 {#if selectedWagen === 'Wagen 1'}
 	<div class="cabin1">
-		<div class="sensorbox">
-			<div class="card">
-				<div class="card-body">
-					<h5>Eingangsbereich</h5>
-					<div class="GUID">GUID: 042743284324242</div>
-					<span class="badge text-bg-success">Connected</span>
-					<br />
-					<br />
-					<button
-						class="btn btn-primary"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#collapseExample"
-						aria-expanded="false"
-						aria-controls="collapseExample"
-					>
-						<span class="material-symbols-outlined"> expand_more </span>
-						3 Sensors associated
-					</button>
+		{#each data.sections as section}
+			<div class="sensorbox">
+				<div class="card">
+					<div class="card-body">
+						<h5>{section.section_name}</h5>
+						<div class="GUID">GUID: {section._id}</div>
+						<span class="badge text-bg-success">Connected</span>
+						<br />
+						<br />
+						<button
+							class="btn btn-primary"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#collapse{section._id}"
+							aria-expanded="false"
+							aria-controls="collapse{section._id}"
+						>
+							<span class="material-symbols-outlined"> expand_more </span>
+							X Sensors associated
+						</button>
+					</div>
 				</div>
+				{#each data.sensors as sensor}
+					{#if sensor.section_id == section._id}
+						<div class="row sensorboxes">
+							<div class="col">
+								<div class="collapse" id="collapse{section._id}">
+									<div class="card card-body">
+										<h6>{sensor.measurement}</h6>
+										<div class="GUID">GUID: {sensor._id}</div>
+										<span class="badge text-bg-success small">{sensor.connection_state}</span>
+										<div class="sensorvalue">{sensor.value} {sensor.unit}</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					{/if}
+				{/each}
 			</div>
-
-			<div class="row sensorboxes">
-				<div class="col">
-					<div class="collapse" id="collapseExample">
-						<div class="card card-body">
-							<h6>Temperatur</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">27.5 °C</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="collapse" id="collapseExample">
-						<div class="card card-body">
-							<h6>Luftfeuchtigkeit</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">71. 3%</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="collapse" id="collapseExample">
-						<div class="card card-body">
-							<h6>CO2 (Luftqualität)</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">432 ppm</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="sensorbox">
-			<div class="card">
-				<div class="card-body">
-					<h5>Sanitärbereich</h5>
-					<div class="GUID">GUID: 042743284324242</div>
-					<span class="badge text-bg-success">Connected</span>
-					<br />
-					<br />
-					<button
-						class="btn btn-primary"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#collapseExample2"
-						aria-expanded="false"
-						aria-controls="collapseExample"
-					>
-						<span class="material-symbols-outlined"> expand_more </span>
-						4 Sensors associated
-					</button>
-				</div>
-			</div>
-
-			<div class="row sensorboxes">
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body">
-							<h6>Temperatur</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">24.5 °C</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body">
-							<h6>Luftfeuchtigkeit</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">86. 3%</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body">
-							<h6>CO2 (Luftqualität)</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">564 ppm</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="row sensorboxes">
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body">
-							<h6>Anzahl Spühlungen</h6>
-							<div class="GUID">GUID: 042743284324242</div>
-							<span class="badge text-bg-success small">Connected</span>
-							<div class="sensorvalue">34</div>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body empty">
-							<h6>x</h6>
-							<div class="GUID"></div>
-							<span class="badge text-bg-success small"></span>
-							<div class="sensorvalue"></div>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="collapse" id="collapseExample2">
-						<div class="card card-body empty">
-							<h6>x</h6>
-							<div class="GUID"></div>
-							<span class="badge text-bg-success small"></span>
-							<div class="sensorvalue"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		{/each}
 	</div>
 {:else if selectedWagen === 'Wagen 2'}
 	<div class="cabin2">
